959efac8449fe0f8c0363049cb87e28526709046
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:49 +0100

    Create ex5.java

---
+++ ex5.java
@@ -0,0 +1,13 @@
+package aula3;
+
+public class ex5 {
+    public static void main(String[] args) {
+        ex5(0);
+    }
+    public static void ex5(int i){
+        while (i<5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
