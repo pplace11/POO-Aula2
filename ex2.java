6e58fe04980f2273e0b7f82fa3083fa9fd3f694f
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:34 +0100

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,14 @@
+package aula3;
+
+public class ex2 {
+   public static void main(String[] args) {
+    ex2(9);
+   }
+   public static void ex2(int i) {
+    if(i<0 && i>10){
+        System.out.println("é maior");
+    }else{
+        System.out.println("é menor");
+    }
+   }
+}
