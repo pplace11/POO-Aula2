aa5172827dee36671502a74d2c0b1daa0823d497
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:53 +0100

    Create ex6.java

---
+++ ex6.java
@@ -0,0 +1,13 @@
+package aula3;
+
+public class ex6 {
+    public static void main(String[] args) {
+        ex6(5);
+    }
+    public static void ex6(int i){
+        while (i>1){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
