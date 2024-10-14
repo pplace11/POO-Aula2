1deef4e5752369cd7f2e0bdd561b247345a59543
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:44 +0100

    Create ex4.java

---
+++ ex4.java
@@ -0,0 +1,14 @@
+package aula3;
+
+public class ex4 {
+    public static void main(String[] args) {
+    ex4(28);
+    }
+    public static void ex4(int i){
+        if(i>0 && i<18){
+            System.out.println("menor de idade");
+        }else if(i>18 && i<50){
+            System.out.println("maior de idade");
+        }
+    }
+}
