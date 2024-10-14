f87b20f965765e4f5a7da477b05fdd62e56b345e
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:39 +0100

    Create ex3.java

---
+++ ex3.java
@@ -0,0 +1,11 @@
+package aula3;
+public class ex3 {
+    public static void main(String[] args) {
+        ex3(15);
+    }
+    public static void ex3(int i) {
+        if(i>10 && i<20){
+            System.out.println("está no inteiro");
+        }
+    }
+}
