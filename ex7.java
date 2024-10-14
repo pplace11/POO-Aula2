cf3147ebbdffa7b7e14a321e92c15b3a616b5f99
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:59 +0100

    Create ex7.java

---
+++ ex7.java
@@ -0,0 +1,12 @@
+package aula3;
+
+public class ex7 {
+    public static void main(String[] args) {
+        ex7(5);
+    }
+    public static void ex7(int n){
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
