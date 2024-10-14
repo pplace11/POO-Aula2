080bcfb7ca27973ade5bbf62f84bd9af80483556
Pedro Place <pedro.place@hotmail.com>
2024-10-14 16:05:31 +0100

    Create ex1.java

---
+++ ex1.java
@@ -0,0 +1,14 @@
+package aula3;
+public class ex1 {
+    public static void main(String[] args) {
+        int i = -1;
+        ex1(i);
+    }
+    public static void ex1 (int i ){
+    if(i>=0){
+        System.out.println("é positivo");
+    }else{
+        System.out.println("é negativo");
+    }
+}
+}
