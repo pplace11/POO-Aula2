07cb5f18c95baa59b84b51271584e60a934e2a0a
Pedro Place <pedro.place@hotmail.com>
2024-10-16 18:25:20 +0100

    Create ex14.java

---
+++ ex14.java
@@ -0,0 +1,12 @@
+package aula3;
+
+public class ex14 {
+    public static void main(String[] args) {
+        ex14(5);
+    }
+    public static void ex14(int numero){
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numero + "x" + i + "=" + (numero * i));
+        }
+    }
+}
