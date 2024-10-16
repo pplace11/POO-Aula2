8336f61ab20a148621cfd0eecc202f1d1130ec9b
Pedro Place <pedro.place@hotmail.com>
2024-10-16 18:25:14 +0100

    Create ex13.java

---
+++ ex13.java
@@ -0,0 +1,17 @@
+package aula3;
+
+public class ex13 {
+    public static void main(String[] args) {
+        ex13();
+    }
+    public static void ex13(){
+        int numero = 1;
+        while (numero<=50){
+        }if (numero%2==0){
+            System.out.println(numero + "é par");
+        }else{
+            System.out.println(numero + "é ímpar");
+        }
+        numero++;
+    }
+}
